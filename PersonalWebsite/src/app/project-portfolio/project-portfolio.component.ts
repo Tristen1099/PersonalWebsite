a1f03a5c279cf33b86c9d6212e5f13be89b042b0
Tristen Rivera <trivera1@my.westga.edu>
2020-02-08 11:36:55 -0500

    Added ngFor for projects and added some starter examples

--- PersonalWebsite/src/app/project-portfolio/project-portfolio.component.ts
+++ PersonalWebsite/src/app/project-portfolio/project-portfolio.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectPortfolioComponent implements OnInit {
 
+  projects = new Array();
+
   constructor() { }
 
   ngOnInit() {
+    this.projects = [{
+      name: "Word Scramble",
+      language: "C++",
+      date: "May, 2019",
+      description: "This word scrabmle game lets the user choose from...."
+
+    }, {
+      name: "Word Scramble",
+      language: "C++",
+      date: "May, 2019",
+      description: "This word scrabmle game lets the user choose from...."
+
+    }, {
+      name: "Word Scramble",
+      language: "C++",
+      date: "May, 2019",
+      description: "This word scrabmle game lets the user choose from...."
+
+    }];
+  }
+
+  ngAfterViewInit() {
+
+    document.getElementById("project-carousel").children[0].classList.add("active");
   }
 
 }
