13da5a63445269c03ce0857dd094444e5ae0b608
Tristen Rivera <trivera1@my.westga.edu>
2020-02-09 16:07:30 -0500

    Added smooth scrolling animation

--- PersonalWebsite/src/app/app.component.ts
+++ PersonalWebsite/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,28 @@ export class AppComponent {
 
   hamburgerClick() {
     const element = document.getElementById("hamburgerIcon");
-    
+
     if (element.classList.contains("is-active")) {
       element.classList.remove("is-active");
     } else {
       element.classList.add("is-active");
     }
   }
+
+
+
 }
+
+$(document).ready(function () {
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+});
