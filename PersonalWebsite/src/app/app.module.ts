608b6aa77aa0146e6f4767e2f30db0d347758b10
Tristen Rivera <trivera1@my.westga.edu>
2020-02-15 20:25:10 -0500

    Added education section

--- PersonalWebsite/src/app/app.module.ts
+++ PersonalWebsite/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroIntroComponent } from './hero-intro/hero-intro.component';
 import { ProjectPortfolioComponent } from './project-portfolio/project-portfolio.component';
+import { EducationHistoryComponent } from './education-history/education-history.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroIntroComponent,
-    ProjectPortfolioComponent
+    ProjectPortfolioComponent,
+    EducationHistoryComponent
   ],
   imports: [
     BrowserModule,
