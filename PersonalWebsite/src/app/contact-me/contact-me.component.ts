69a252a492f6254f523e349b2371621db65a4bd1
Tristen Rivera <trivera1@my.westga.edu>
2020-02-19 21:14:36 -0500

    Added basic contact form

--- PersonalWebsite/src/app/contact-me/contact-me.component.ts
+++ PersonalWebsite/src/app/contact-me/contact-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-contact-me',
@@ -12,4 +13,22 @@ export class ContactMeComponent implements OnInit {
   ngOnInit() {
   }
 
+  submitForm(event: any) {
+
+    const contactName = (document.getElementById('formName') as HTMLInputElement).value;
+    const contactEmail = (document.getElementById('formEmail') as HTMLInputElement).value;
+    const contactMessage = (document.getElementById('formMessage') as HTMLInputElement).value;
+
+    event.preventDefault();
+
+    $.post('https://script.google.com/macros/s/AKfycbyN4nzL1LykXY4V9SvUSbtmPX3JvlBO3ds3mKtdcVZyIag6CRI/exec', {
+      name: contactName,
+      email: contactEmail,
+      message: contactMessage
+    }).done(function () {
+      console.log("Success");
+    }).fail(function () {
+      console.log("Fail")
+    });
+  }
 }
