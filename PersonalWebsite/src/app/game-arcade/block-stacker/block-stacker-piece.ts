b8761648223172172530622aff2e633fee0c5cc6
Tristen Rivera <trivera1@my.westga.edu>
2020-08-16 14:10:45 -0400

    Created Game board with style. Added button to add a game piece to the canvas

---
+++ PersonalWebsite/src/app/game-arcade/block-stacker/block-stacker-piece.ts
@@ -0,0 +1,38 @@
+export interface IPiece {
+    x: number;
+    y: number;
+    color: string;
+    shape: number[][];
+}
+
+export class Piece implements IPiece {
+    x: number;
+    y: number;
+    color: string;
+    shape: number[][];
+
+    constructor(private canvasContext: CanvasRenderingContext2D) {
+        this.spawn();
+    }
+
+    spawn() {
+        this.color = 'green';
+        this.shape = [[0, 1, 1], [1, 1, 0], [0, 0, 0]];
+
+        this.x = 3;
+        this.y = 1;
+    }
+
+    draw() {
+        this.shape.forEach((row, y) => {
+            row.forEach((value, x) => {
+                if (value > 0) {
+                    this.canvasContext.fillStyle = this.color;
+                    const currentX = this.x + x;
+                    const currentY = this.y + y;
+                    this.canvasContext.fillRect(currentX, currentY, 1, 1);
+                }
+            });
+        });
+    }
+}
--- PersonalWebsite/src/app/game-arcade/block-stacker/block-stacker.component.ts
+++ PersonalWebsite/src/app/game-arcade/block-stacker/block-stacker.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { COLS, BLOCK_SIZE, ROWS } from './block-stacker-constants';
+import { Piece, IPiece } from './block-stacker-piece';
 
 @Component({
   selector: 'app-block-stacker',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlockStackerComponent implements OnInit {
 
+  @ViewChild('board', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+  canvasContext: CanvasRenderingContext2D;
+
+  level: number;
+  score: number;
+  lines: number;
+
+  currentPiece: Piece;
+  gameBoard: number[][];
+
+
   constructor() { }
 
   ngOnInit() {
+    this.initializeBoard();
+    
+    this.resetGame();
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      AppComponent.mainPage = false;
+    });
+  }
+
+  initializeBoard() {
+    this.canvasContext = this.canvas.nativeElement.getContext('2d');
+    this.canvasContext.canvas.width = COLS * BLOCK_SIZE;
+    this.canvasContext.canvas.height = ROWS * BLOCK_SIZE;
+    this.canvasContext.scale(BLOCK_SIZE, BLOCK_SIZE);
+  }
+
+  play() {
+    this.resetGame();
+    this.currentPiece = new Piece(this.canvasContext);
+    this.currentPiece.draw();
+  }
+
+  private resetGame() {
+    this.score = 0;
+    this.lines = 0;
+    this.level = 1;
+    this.gameBoard = this.getEmptyGameBoard();
+  }
+
+  private getEmptyGameBoard() {
+    return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
   }
 
 }
