b1a731723ec1f04a3e307048275b7b59eb5e61c4
Tristen Rivera <trivera1@my.westga.edu>
2020-08-25 17:45:06 -0400

    Removed import

--- PersonalWebsite/src/app/game-arcade/bomb-duster/bomb-duster.component.ts
+++ PersonalWebsite/src/app/game-arcade/bomb-duster/bomb-duster.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, ɵConsole } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 import { Cell } from './cell';
 import { CellStatus, NEIGHBORS } from './cell';
