1c09dec707256527ca19d32f1b203339b60ac507
Tristen Rivera <trivera1@my.westga.edu>
2021-11-02 17:40:01 -0400

    Added more work history and transitions for viewing details

--- PersonalWebsite/src/app/work-history/work-history.component.ts
+++ PersonalWebsite/src/app/work-history/work-history.component.ts
@@ -12,4 +12,26 @@ export class WorkHistoryComponent implements OnInit {
   ngOnInit() {
   }
 
+  articleHover(element) {
+    let articleContent = element.getElementsByClassName("articleContent")[0];
+    let subDetails = articleContent.getElementsByClassName("subDetails");
+    for(let index = 0; index < subDetails.length; index++) {
+      subDetails[index].style.transition = "max-height 1.1s, opacity .5s";
+      subDetails[index].style.maxHeight = "200px";
+      subDetails[index].style.opacity = "1";
+      
+    }
+  }
+
+  articleLeave(element) {
+    let articleContent = element.getElementsByClassName("articleContent")[0];
+    let subDetails = articleContent.getElementsByClassName("subDetails");
+    for(let index = 0; index < subDetails.length; index++) {
+      subDetails[index].style.transition = "max-height .7s, opacity .5s";
+      subDetails[index].style.maxHeight = "0";
+      subDetails[index].style.opacity = "0";
+    }
+  }
+
+
 }
